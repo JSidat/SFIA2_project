dc3768f2a6927929b433fa9fe58b1e2d7597e1e0
JSidat <JSidat@qa.com>
2020-06-08 19:06:49 +0000

    app changes

--- service_2/application/routes.py
+++ service_2/application/routes.py
@@ -2,8 +2,20 @@ from application import app
 from flask import request, Response
 from random import randint
 
-@app.route('/city/name', methods = ['GET'])
+@app.route('/country/name', methods = ['GET'])
 def country():
     countries = ['Italy', 'Japan', 'Brazil']
-    Country = (cities[randint(0, len(list)-1)], mimetype= 'text/plain')
+    Country = (cities[randint(0, len(countries)-1)], mimetype= 'text/plain')
     return Country
+
+@app.route('country/coordinates', methods = ['GET'])
+def coordinates():
+    coordinates = ['36.2048° N 138.2529° E', '41.8719° N 12.5674° E', '14.2350° S 51.9253° W']
+    coordinate = (coordinates[randint(0, len(coordinates)-1)], mimetype='text/plain')
+    if coordinate == coordinates[0]:
+        country = 'Japan'
+    elif coordinate == coordinates[1]:
+        country = 'Italy'
+    elif coordinate == coordinates[2]:
+        country = 'Brazil'
+    return country
--- service_3/application/routes.py
+++ service_3/application/routes.py
@@ -2,10 +2,15 @@ from application import app
 from flask import request, Response
 from random import randint
 
-@app.route('/city/country', methods = ['POST'])
-def topic():
-    topics = ['population', 'continent', 'main language']
-    topic = (topics[randint(0, len(topics)-1)], mimetype='text/plain')
-    return topic
+@app.route('/country/population', methods = ['POST'])
+def population():
+    numbers = ['60.36 million', '209.5 million', '126.5 million']
+    population = (numbers[randint(0, len(topics)-1)], mimetype='text/plain')
+    return population
     #country =  request.data.decode('utf-8')
-    
+
+@app.route('/country/continent', methods = ['POST'])
+def continent():
+    continents = ['South America', 'Asia', 'Europe']
+    continent = (continents[randint(0, len(continents)-1)], mimetype='plain/text')
+    return continent
