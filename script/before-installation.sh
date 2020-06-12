667ca866a55dc6fe2a2c13bccecc2491c081afa2
JSidat <JSidat@qa.com>
2020-06-12 09:02:40 +0000

    typo

--- script/before-installation.sh
+++ script/before-installation.sh
@@ -7,7 +7,7 @@ apt install -y python-pip
 
 mkdir -p ~/.local/bin
 touch ~/.bashrc
-echo 'PATH=$PATH:~/.local/bin' > ~/.bashrc
+echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
 
 sudo chown -R $(whoami):$(whoami) ~/*
 source ~/.bashrc
